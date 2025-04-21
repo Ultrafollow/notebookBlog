d866791d4df0b0e5bbb9a9b3802b325caee080fc
Ultrafollow <1368744884@qq.com>
2025-04-21 17:44:05 +0800

    fix_webpack

--- next.config.js
+++ next.config.js
@@ -1,16 +1,31 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   experimental: {
+//     turbo: {
+//       rules: {
+//         '*.svg': {
+//           loaders: ['@svgr/webpack'],
+//           as: '*.js',
+//         },
+//       },
+//     },
+//   },
+// }
 const nextConfig = {
-  /* config options here */
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
+  webpack: (config) => {
+    // 添加 SVG 处理规则
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+    return config;
   },
+  // 允许 Next.js 的 Image 组件处理 SVG
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+  }
 }
  
 module.exports = (nextConfig)
