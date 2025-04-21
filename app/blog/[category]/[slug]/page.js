2e2fbc6fcdb0474195dad005669956e99accf1b0
Ultrafollow <1368744884@qq.com>
2025-04-21 19:13:01 +0800

    fix_page3

--- app/blog/[category]/[slug]/page.js
+++ app/blog/[category]/[slug]/page.js
@@ -24,9 +24,9 @@ registerPrismLanguages()
 // 获取解码后的分类目录
 export async function generateStaticParams() {
   const categoriesData = await getCategoriesWithPosts()
-  const { category,posts } = categoriesData[0]
-  console.log(decodeURIComponent(category))
-  console.log(posts[0].slug)
+  // const { category,posts } = categoriesData[0]
+  // console.log(decodeURIComponent(category))
+  // console.log(posts[0].slug)
   return categoriesData.flatMap(({ category, posts }) => 
     posts.map(post => ({
       category: decodeURIComponent(category),
