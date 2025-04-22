dcbb5b118778fef323f4e16588bea0f5ed7dbb5e
Ultrafollow <1368744884@qq.com>
2025-04-23 01:45:20 +0800

    add seo3

--- app/layout.jsx
+++ app/layout.jsx
@@ -44,6 +44,7 @@ export default function RootLayout({ children }) {
     >
       <head>
         <meta name="baidu-site-verification" content="codeva-MMXplx3EG1" />
+        <meta name="msvalidate.01" content="E6102F9220389FDC31EFF5A009FDE6A5" />
       </head>
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
