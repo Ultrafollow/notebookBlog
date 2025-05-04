f18bd02dea86dc58773762aa82e05346c40c3b8c
Ultrafollow <1368744884@qq.com>
2025-05-04 16:05:10 +0800

    loading

--- app/loading.js
+++ app/loading.js
@@ -20,18 +20,18 @@ function Logo() {
 
 
 export default function Loading() {
-    return (
-      <div className="flex h-screen w-full flex-col items-center justify-center bg-white dark:bg-gray-900">
-        <div className="mb-8 flex flex-1 items-end animate-bounce">
-          <Logo />
-        </div>
-        <div className="flex flex-1 items-start">
-          <div className="flex space-x-2">
-            <div className="h-3 w-3 animate-pulse rounded-full bg-blue-600"></div>
-            <div className="h-3 w-3 animate-pulse rounded-full bg-blue-600 animation-delay-150"></div>
-            <div className="h-3 w-3 animate-pulse rounded-full bg-blue-600 animation-delay-300"></div>
-          </div>
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-white dark:bg-gray-900">
+      <div className="mb-8 flex flex-1 items-end animate-bounce">
+        <Logo />
+      </div>
+      <div className="flex flex-1 items-start">
+        <div className="flex space-x-2">
+          <div className="h-3 w-3 animate-pulse rounded-full bg-blue-600"></div>
+          <div className="h-3 w-3 animate-pulse rounded-full bg-blue-600 animation-delay-150"></div>
+          <div className="h-3 w-3 animate-pulse rounded-full bg-blue-600 animation-delay-300"></div>
         </div>
       </div>
-    )
+    </div>
+  )
   }
