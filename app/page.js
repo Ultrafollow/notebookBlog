63687de799037bb5ea969ff6e71322df92577f3e
follow <1368744884@qq.com>
2025-05-09 16:47:59 +0800

    seo

--- app/page.js
+++ app/page.js
@@ -2,6 +2,13 @@ import { Home } from '@/components/home-page'
 export async function generateMetadata({ params }) {
   return {
     title: 'Follow XU',
+    description: 'Follow XU 的个人博客，分享技术文章和生活点滴',
+    openGraph: {
+      title: 'Follow XU',
+      description: 'Follow XU 的个人博客，分享技术文章和生活点滴',
+      type: 'website',
+      url: 'https://followxu.top',
+    }
   }
 }
 export default async function HomePage() {
