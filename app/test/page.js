dbd748562db7f396b0d87cf5ef667b8b1571da60
Ultrafollow <1368744884@qq.com>
2025-05-17 16:48:58 +0800

    supabase

--- app/test/page.js
+++ app/test/page.js
@@ -1,10 +1,8 @@
-// app/test/page.jsx
-export default function Test() {
-    return (
-      <div className="container mx-auto p-4 bg-red-100">
-        <h1 className="text-3xl font-bold text-blue-600">
-          Tailwind 测试
-        </h1>
-      </div>
-    )
-  }
+import { createClient } from '@/utils/supabase/server';
+
+export default async function Notes() {
+  const supabase = await createClient();
+  const { data: notes } = await supabase.from("notes").select();
+
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>
+}
