42c03a959809a85173b3764ad5f6041113c938f6
Ultrafollow <1368744884@qq.com>
2025-05-24 17:10:08 +0800

    env

--- components/Author/CheckKey.js
+++ components/Author/CheckKey.js
@@ -1,4 +1,4 @@
-export default function CheckKey({id}) {
+export default async function CheckKey({id}) {
   const Ids = process.env.ALLOWED_USERS_ID?.split(',') || [];
   console.log('Allowed IDs:', Ids);
   console.log('DEFAULT_SESSION_ID:',process.env.DEFAULT_SESSION_ID)
