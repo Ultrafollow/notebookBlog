b91ee5798a4669ed657ddf95810090e1de00add8
Ultrafollow <1368744884@qq.com>
2025-05-24 16:38:38 +0800

    id

--- components/Author/CheckKey.js
+++ components/Author/CheckKey.js
@@ -1,6 +1,6 @@
-
 export default function CheckKey({id}) {
   const Ids = process.env.ALLOWED_USERS_ID?.split(',') || [];
+  console.log('Allowed IDs:', Ids);
   const isAllowed = Ids.includes(id);
 
     return isAllowed;
