7e5b0b588c6d7c4ac2b37ec12aadf0c31c7a53c8
Ultrafollow <1368744884@qq.com>
2025-06-17 07:45:18 +0800

    delete

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
-import { generateStableId } from "@/app/lib/utils"
+// import { generateStableId } from "@/app/lib/utils"
 
 
 export const { handlers, auth, signIn, signOut } = NextAuth({ 
